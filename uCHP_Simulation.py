4668e463c00d885309f1ae7f12704368b1a7aa09
Dr. David Fritz <dlfritz@gmail.com>
2015-07-28 09:35:38 +0200

    Added the boundry conditions input file: uCHP_Randbedignung.dat Added the main simulation file: uCHP_Simulation.py Added the heat transfer simulation for thermal storage: uCHP_Thermalspeicher.py

---
+++ uCHP_Simulation.py
@@ -0,0 +1,30 @@
+
+# Simulation written by: Dr. David Fritz
+# Created on: 25.07.2015
+# Last Modified: 25.07.2015
+#
+# Purpose of this function:
+# This function is designed to simulate a µCHP system with various configurations.
+# The available configurations are (will include) electric generation capabilities
+# through a hybrid high temp fuel cell, PEM fuel cell coupled with either an
+# internal combustion engine, a sterling engine or a gas turbine. Alternatively
+# the fuel cell component can also be completely removed and a standard internal
+# combustion engine, sterling engine or gas turbine can be used for power
+# generation.
+#
+# Stand of Simulation:
+#                       Under development/Not functioning
+#
+# Work to be completed:
+#                       -Develop outline and flow chart
+#                       -Determine desired inputs and outputs
+#                       -Focus on modular coding to promote easy model replacement and cross-functionality
+#                       -Develop individual component modules
+#                           -Fuel Cell
+#                           -Compressor
+#                           -Turbine...etc.
+#
+#
+#
+# Beginning of Code:
+
---
+++ uCHP_Thermalspeicher.py
@@ -0,0 +1,36 @@
+# Simulation written by: Dr. David Fritz
+# Created on: 25.07.2015
+# Last Modified: 25.07.2015
+#
+# Stand of Simulation:
+#                       Under development/Not functioning
+#
+# Work to be completed:
+#                       -Implement heat transfer/loss simulations
+#                       -List function dependent variables
+#                       -Überlege Funktionalität für den Wärmeverlust des
+#                       Rohrleitung
+# Thermalspeicher.py Funktion ist allerdings erst für die Wärmetransport von
+# Wasserspeichertanken verantwortlich. In dieser Funktion ist die folgender
+# Variablen verwendet.
+#
+# GLOBAL VARIABLEN:
+#
+#
+#
+# LOKALEN VARIABLEN:
+#
+#
+#
+# INPUT VARIABLEN:
+#
+#
+# OUTPUT VARIABLEN:
+#
+#
+#
+# Beginning of the function:
+
+
+
+
